c1129357ccc545b7f24a91d8a9f86d61e6086d23
MrDogukan <aliseslikaya@gmail.com>
2022-01-02 16:19:39 +0300

    v3

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,26 +1,24 @@
-import "./Cards.css"
-import {data} from "../../helpers/data";
-
-
+import React from "react";
+import { data } from "../../helpers/data";
 
 const Cards = () => {
-    return (
-        <div className = "card-container">
-           {data.map((card,index)=>{
-                return(
-                    <div className="cards" key={index}>
-                        <div className= "title">
-                            <h2>{card.title}</h2>
-                        </div>
-                        <img src={card.image} alt={card.title}/>
-                        <div className="card-over">
-                            <p>{card.desc}</p>
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="card-container">
+      {data.map((card, index) => {
+        return (
+          <div className="cards" key={index}>
+            <div className="title">
+              <h2>{card.title}</h2>
+            </div>
+            <img src={card.image} alt={card.title} />
+            <div className="card-over">
+              <p>{card.desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default Cards
+export default Cards;
